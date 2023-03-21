dcb9093f52083e58258980888210b54653c00cd6
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 14:03:01 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Läs in ett godtyckligt antal tal (double) till en lista, och skicka sen listan
+        // till en metod isSorted som returnerar true om talen är sorterade i stigande ordning
+
+
+    }
+}
